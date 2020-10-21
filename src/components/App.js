9696ac97822871bd813961a4a853bc48cf3bae41
aravindbhaskar41 <aravindbhaskar41@gmail.com>
2020-10-21 20:36:28 +0530

    requirements completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,28 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
-
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  state = {
+    isButtonClicked: false
+  };
+  render() {
+    return (
+      <div className="App">
+        <button
+          id="click"
+          onClick={() => this.setState({ isButtonClicked: true })}
+        >
+          Click me to render para
+        </button>
+        {this.state.isButtonClicked === true ? (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        ) : null}
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
