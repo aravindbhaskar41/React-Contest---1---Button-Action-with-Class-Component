8374ad3c392774fc64aee3eee430acc2a8e76140
aravindbhaskar41 <aravindbhaskar41@gmail.com>
2020-10-21 21:20:31 +0530

    change in logic

--- src/components/App.js
+++ src/components/App.js
@@ -3,24 +3,23 @@ import "../styles/App.css";
 
 class App extends Component {
   state = {
-    isButtonClicked: false
+    para: ""
   };
   render() {
     return (
       <div id="main">
         <button
           id="click"
-          onClick={() => this.setState({ isButtonClicked: true })}
+          onClick={() =>
+            this.setState({
+              para: `Hello, I've learnt to use the full-stack evaluation tool. This makes
+		  me so happy`
+            })
+          }
         >
           Click me to render para
         </button>
-        {this.state.isButtonClicked === true ? (
-          <p id="para">
-            Hello, I've learnt to use the full-stack evaluation tool. This makes
-            me so happy
-          </p>
-        ) : (
-          <p id="para"></p>
+        <p id="para">{this.state.para}</p>
         )}
       </div>
     );
